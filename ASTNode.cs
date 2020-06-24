c6902c7421cda23689f5a6875f2de232b10d1dfa
unknown <murali.isu@gmail.com>
2020-06-24 17:54:56 +0530

    Representing AST object as JSON & printing in console.

---
+++ ASTNode.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Task_1
+{
+[Serializable]
+    public class ASTNode
+    {
+        public string Name { get; }
+
+        public IDictionary<string, string> Properties { get; }
+        public IList<ASTNode> Children { get; } = new List<ASTNode>();
+
+        public ASTNode(IDictionary<string, string> properties) =>
+            (Properties, Name) = (properties, properties.Values.First());
+
+        public void AddChild(ASTNode child)
+        {
+            Children.Add(child);
+        }
+
+        public override string ToString()
+        {
+            return Properties.Values.First();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
 
 namespace Task_1
 {
@@ -10,7 +12,7 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            //Accessig Files under "Examples" directory
+            //Accessing Files under "Examples" directory
             try
             {
                 string curDir=Directory.GetCurrentDirectory()+"\\Examples";
@@ -20,125 +22,55 @@ namespace Task_1
                 {
                     string programLines = File.ReadAllText(fileName);
                     //Forming Syntax Tree
-                    SyntaxTree syntaxTree= CSharpSyntaxTree.ParseText(programLines);
-                    CompilationUnitSyntax root= syntaxTree.GetCompilationUnitRoot();
-
-                    //Printing Members in Program
-                    Console.WriteLine("Usings :"+root.Usings.Count);
-                    foreach (UsingDirectiveSyntax usingDirective in root.Usings)
-                        Console.WriteLine("Using : "+usingDirective.Name);
-
-                    //Namespace Declarartion
-                    MemberDeclarationSyntax firstMember = root.Members[0];
-                    var namespaceDeclarationSyntax = (NamespaceDeclarationSyntax)firstMember;
-
-                        Console.WriteLine("Namespace :"+ namespaceDeclarationSyntax.Name);
-                    Console.WriteLine(namespaceDeclarationSyntax.Members.Count+" members declared in this namespace.");
-                    //looping all members under namespace
-                    foreach (var classes in namespaceDeclarationSyntax.Members)
-                    {
-                        if(classes.Kind()== SyntaxKind.ClassDeclaration)
-                        {
-                            //Parsing of Class
-                            ClassDeclarationSyntax classDeclarationSyntax=(ClassDeclarationSyntax)classes;
-                            Console.WriteLine("\nClass :"+classDeclarationSyntax.Identifier);
-                            foreach (var classMember in classDeclarationSyntax.Members)
-                            {
-                                switch(classMember.Kind())
-                                {
-                                    case SyntaxKind.MethodDeclaration :
-                                    //Parsing of Methods
-                                        MethodDeclarationSyntax method= classMember as MethodDeclarationSyntax;
-                                        Console.WriteLine("Method : "+method.Identifier+" , "+method.ReturnType);
-                                        foreach (ParameterSyntax item in method.ParameterList.Parameters)
-                                            Console.WriteLine("  Parameter : "+item.Identifier+" , "+item.Type);
-                                        Console.WriteLine("  Body Length : "+ method.Body.ToFullString().Length);
-                                    break;
-                                    case SyntaxKind.FieldDeclaration:
-                                    //Parsing of Fields
-                                        FieldDeclarationSyntax field = classMember as FieldDeclarationSyntax;
-                                        foreach (VariableDeclaratorSyntax item in field.Declaration.Variables)
-                                            Console.WriteLine("Field : "+item.Identifier+" , " +field.Declaration.Type);
-                                    break;
-                                    case SyntaxKind.PropertyDeclaration:
-                                    //Parsing of Properties
-                                        PropertyDeclarationSyntax property = classMember as PropertyDeclarationSyntax;
-                                        Console.WriteLine("Property : "+property.Identifier+" , " +property.Type);
-                                    break;
-                                }
-                            }
-                            Console.WriteLine();
-                        }
-                        else if(classes.Kind()== SyntaxKind.InterfaceDeclaration)
-                        {
-                            //Parsing Interface
-                            InterfaceDeclarationSyntax interfaceDeclarationSyntax=(InterfaceDeclarationSyntax)classes;
-                            Console.WriteLine("Interface :"+interfaceDeclarationSyntax.Identifier);
-                            foreach (var interfaceMember in interfaceDeclarationSyntax.Members)
-                            {
-                                switch(interfaceMember.Kind())
-                                {
-                                    //Parsing of Methods
-                                    case SyntaxKind.MethodDeclaration :
-                                        MethodDeclarationSyntax method= interfaceMember as MethodDeclarationSyntax;
-                                        Console.WriteLine("Method : "+method.Identifier+" , "+method.ReturnType);
-                                        foreach (ParameterSyntax item in method.ParameterList.Parameters)
-                                            Console.WriteLine("  Parameter : "+item.Identifier+" , "+item.Type);
-                                    break;
-                                    case SyntaxKind.PropertyDeclaration:
-                                    //Parsing of Properties
-                                        PropertyDeclarationSyntax field = interfaceMember as PropertyDeclarationSyntax;
-                                        Console.WriteLine("Field : "+field.Identifier+" , " +field.Type);
-                                    break;
-                            }
-                            }
-                        }
-                        else if(classes.Kind()== SyntaxKind.StructDeclaration)
-                        {
-                            //Parsing Structs
-                            StructDeclarationSyntax structDeclarationSyntax=(StructDeclarationSyntax)classes;
-                            Console.WriteLine("\nStruct :"+structDeclarationSyntax.Identifier);
-                            foreach (var structMember in structDeclarationSyntax.Members)
-                            {
-                                switch(structMember.Kind())
-                                {
-                                    case SyntaxKind.MethodDeclaration :
-                                    //Parsing of Methods
-                                        MethodDeclarationSyntax method= structMember as MethodDeclarationSyntax;
-                                        Console.WriteLine("Method : "+method.Identifier+" , "+method.ReturnType);
-                                        foreach (ParameterSyntax item in method.ParameterList.Parameters)
-                                            Console.WriteLine("  Parameter : "+item.Identifier+" , "+item.Type);
-                                        Console.WriteLine("  Body Length : "+method.Body.ToFullString().Length);
-                                    break;
-                                    //Parsing of Fields
-                                    case SyntaxKind.FieldDeclaration:
-                                        FieldDeclarationSyntax fieldDeclarationSyntax= structMember as FieldDeclarationSyntax;
-                                        foreach (VariableDeclaratorSyntax item in fieldDeclarationSyntax.Declaration.Variables)
-                                            Console.WriteLine("Field : "+item.Identifier+" , "+fieldDeclarationSyntax.Declaration.Type);
-                                    break;
-                                    //Parsing of Properties
-                                    case SyntaxKind.PropertyDeclaration:
-                                        PropertyDeclarationSyntax property = structMember as PropertyDeclarationSyntax;
-                                        Console.WriteLine("Property : "+property.Identifier+" , " +property.Type);
-                                    break;
-                                    //Parsing of EventFields
-                                    case SyntaxKind.EventFieldDeclaration:
-                                        EventFieldDeclarationSyntax eventField = structMember as EventFieldDeclarationSyntax;
-                                        foreach (VariableDeclaratorSyntax item in eventField.Declaration.Variables)
-                                            Console.WriteLine("EventField : "+item.Identifier+" , "+eventField.Declaration.Type);
-                                    break;
-                               }
-                            }
-                        }
-                    }
-                    //End pf Each File
-                    Console.WriteLine("---------------------------------");
+                    SyntaxNode syntaxNode= CSharpSyntaxTree.ParseText(programLines).GetRoot();
+                    ASTNode root= CreateSyntaxTree(syntaxNode);
+                    //Serialize & Print
+                    Console.WriteLine(JsonConvert.SerializeObject(root));
                 }
             }
             catch (System.Exception ex)
             {
-                Console.WriteLine("Error Occurred"+ex.Message);
+                Console.WriteLine("\nError Occurred : \n"+ex.StackTrace);
             }
+        }
+        private static ASTNode CreateSyntaxTree(SyntaxNodeOrToken nodeOrToken)
+        {
+            var root = new ASTNode(GetSyntaxNodeOrTokenInfo(nodeOrToken));
+            foreach (SyntaxNodeOrToken child in nodeOrToken.ChildNodesAndTokens())
+            {
+                root.AddChild(CreateSyntaxTree(child));
+            }
+            return root;
+        }
+        private static IDictionary<string, string> GetSyntaxNodeOrTokenInfo(SyntaxNodeOrToken nodeOrToken)
+        {
+            return nodeOrToken.IsNode
+                ? GetSyntaxInfo(nodeOrToken.AsNode())
+                : GetSyntaxInfo(nodeOrToken.AsToken());
+        }
+        private static IDictionary<string, string> GetSyntaxInfo<T>(T syntax)
+        {
+            var result = new Dictionary<string, string>();
+            if (syntax is SyntaxNode node)
+            {
+                result.Add("NodeKind", node.Kind().ToString());
+            }
+            else if (syntax is SyntaxToken token)
+            {
+                result.Add("TokenKind", token.Kind().ToString());
+            }
+            PropertyInfo[] properties = syntax.GetType().GetProperties();
+            foreach (PropertyInfo info in properties)
+            {
+
+                if (info.Name == "Language" || info.Name == "Parent" || info.Name == "ValueText"
+                || info.Name == "Value" || info.Name == "SyntaxTree" || info.Name == "RawKind")
+                {
+                    continue;
+                }
+                result.Add(info.Name, info.GetValue(syntax)?.ToString());
+            }
+            return result;
         }
     }
 }
